b39671b0ce152fd80b9cd98932b33d535c96012c
wuzhe <1038764857@qq.com>
2017-11-06 18:07:28 +0800

    扫描仪连接测试，浮点数案例编写

---
+++ 畅飞扬档案管理系统/Resources/dynamsoft.webtwain.config.js
@@ -0,0 +1,34 @@
+﻿//
+// Dynamsoft JavaScript Library for Basic Initiation of Dynamic Web TWAIN
+// More info on DWT: http://www.dynamsoft.com/Products/WebTWAIN_Overview.aspx
+//
+// Copyright 2017, Dynamsoft Corporation 
+// Author: Dynamsoft Team
+// Version: 13.2
+//
+/// <reference path="dynamsoft.webtwain.initiate.js" />
+var Dynamsoft = Dynamsoft || { WebTwainEnv: {} };
+
+Dynamsoft.WebTwainEnv.AutoLoad = true;
+
+///
+Dynamsoft.WebTwainEnv.Containers = [{ContainerId:'dwtcontrolContainer', Width:270, Height:350}];
+
+/// If you need to use multiple keys on the same server, you can combine keys and write like this 
+/// Dynamsoft.WebTwainEnv.ProductKey = 'key1;key2;key3';
+Dynamsoft.WebTwainEnv.ProductKey = 'DF1C1F2EC339780955AB62BB4FB7580BECB366272083D8747EDC6BD44CDA8EB99A8216C6D8ADC952E49FFE9B4B96E3759A8216C6D8ADC952B2D107DADF08F478ECB366272083D87454543EF1A24827C040000000;t0068WQAAAJm22yI5u4aJ2lpikozrHhvlNA748x5wT5E5Sht9Vzv2M6AFjYMA8jsnoxd9YQLsHLM1gg2EO+NaAUKnEklT0N4=';
+
+///
+Dynamsoft.WebTwainEnv.Trial = true;
+
+///
+Dynamsoft.WebTwainEnv.ActiveXInstallWithCAB = false;
+
+///
+// Dynamsoft.WebTwainEnv.ResourcesPath = 'Resources';
+
+/// All callbacks are defined in the dynamsoft.webtwain.install.js file, you can customize them.
+// Dynamsoft.WebTwainEnv.RegisterEvent('OnWebTwainReady', function(){
+// 		// webtwain has been inited
+// });
+
